e3f141c9057b669de198301f6d4c941fdc6d6d08
nepsoul <nirumagar901@gmail.com>
2024-01-04 15:50:06 +0545

    chore: use of tailwind css

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import AddtoCart from "./AddToCart";
-import styles from "./ProductCard.module.css";
 
 const ProductCard=()=>{
     return (
-        <div className={styles.card}><AddtoCart/></div>
+        <div className='p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-500'>
+            <AddtoCart/>
+            </div>
         )
 }
 
